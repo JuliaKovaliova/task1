aac965d54e6f4f58f5550b414a4698427dbde896
Julia <log6117@yandex.ru>
2023-06-28 23:25:06 +0300

    task2

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задайте массив заполненный случайными положительными трёхзначными числами.
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
-
+/*
 int [] CreateArray (int size)
 {
     int [] randomArray = new int[size];
@@ -38,3 +38,51 @@ int [] firstArray = CreateArray(size);
 PrintArray(firstArray);
 Console.WriteLine();
 FindEvenNumber(firstArray);
+*/
+
+// Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int [] CreateArray (int size)
+{
+    int [] randomArray = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        randomArray[i] = new Random().Next(100,1000);
+    }
+    return randomArray;
+}
+
+void PrintArray (int [] arrayToPrint)
+{
+    Console.WriteLine("Array is: ");
+    for (int i = 0; i < arrayToPrint.Length; i++)
+        Console.Write (arrayToPrint[i] + " ");
+}
+
+void FindSumOddNumber (int [] currentArray)
+{
+    int sum = 0;
+    for (int i = 0; i < currentArray.Length; i++)
+    {
+        if (i % 2 == 1)
+        {
+            sum = sum + currentArray[i];
+        }
+    }
+    Console.WriteLine("Sum of odd positions: " + sum);
+}
+
+int size = 10;
+int [] firstArray = CreateArray(size);
+PrintArray(firstArray);
+Console.WriteLine();
+FindSumOddNumber(firstArray);
+
+
+// Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
